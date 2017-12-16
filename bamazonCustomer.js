520e02d0286f5ee029e54db575a128c784a9eae4
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-16 15:36:23 -0700

    Total price added, video demonstration added, database updated, logs and sensitive info removed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -6,7 +6,7 @@ var inquirer = require("inquirer");
 var connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: 'L1ttl3.Ch13f',
+    password: "",
     database: 'bamazonDB'
   });
 
@@ -65,7 +65,6 @@ var connection = mysql.createConnection({
           //validate the inputs as numbers
       ]).then(function(answers) {
           //"answers" is an object with two key: value, pairs
-        //   console.log(answers);
           var productID = answers.productID;
           var orderQty = answers.orderQty;
           var chosenProduct;
@@ -74,22 +73,14 @@ var connection = mysql.createConnection({
           // allProducts is the param passed into this function. It's full, quantified value is the "results" from the connection query when this function is called and results is passed into the initiateOrder() function.
           for (var i = 0; i < allProducts.length; i++){
               if(productID == allProducts[i].ID){
-                  // console.log(productID);
                   chosenProduct = allProducts[i];
-                  // console.log(orderQty);
-                  console.log(chosenProduct);
               // Two layers of conditional:
               // 1. If the validation in inquirer passes a productID value that we have in the database, do something
                     //if product chosen has sufficient quantity to fulfill the order
-                    // console.log(chosenProduct + " is defined here 1");
-                    // console.log("into conditional for validating if chosen product exists")
                     if(orderQty <= chosenProduct.stock_quantity){
-                      // console.log("into conditional for verifying orderQty is less than stock_qty")
-                      // console.log(chosenProduct.product_name + " is defined here 2");
                       console.log("\nUpdating stock...\n");
-                      // console.log(chosenProduct.product_name);
                       var updatedStock = chosenProduct.stock_quantity - orderQty;
-                      console.log(updatedStock);
+                      var totalPrice = chosenProduct.price * orderQty;
                       //mega scoping issue. This works when the block of code is inside of this, but previously, chosenProduct variable was unavailable to the updateProduct() function. ....... not sure. it's working now so I'll leave it. I wonder if passing the allProducts into the updateProduct() and re-declaring chosenProduct somehow... but the allProducts[i] might also be limited. ARGHGHGHGHGH
 
                       var query = connection.query(
@@ -97,48 +88,24 @@ var connection = mysql.createConnection({
                         [updatedStock, productID],
                     
                         function(err,res) {
-                          console.log(`Thanks. Your ${orderQty} ${chosenProduct.product_name} have been ordered.`);
+                          console.log(`Thanks. Your ${orderQty} ${chosenProduct.product_name} have been ordered. Your order total is $${totalPrice}.`);
                           connection.end();
                         }
                       );
                     }else{
-                      // console.log(chosenProduct + " is defined here 3");
                       console.log(`\nSorry, we have ${chosenProduct.stock_quantity} ${chosenProduct.product_name} available.\n`)
                       connection.end();
                     }
-                  
-              // and later
-              //2. If the product id is actually present
-              // if the validation in inquirer allows a number, but it is a number corresponding to a nonexistent product
               }
           }
           
       })
   };
-  // does connection.end() get called automatically at the end of the function? here, I don't call it, but the function ends and the connection is just over.
+
 
 
   //update conditionals to set "have" or "has" -- been ordered, where orderQty is > or < 1.
   //commented out because it was causing scoping issues ---> not working.
-  // function updateProduct(){
-  //   console.log("\nUpdating stock...\n");
-  //   console.log(chosenProduct.product_name);
-  //   var updatedStock = chosenProduct.stock_quantity - orderQty;
-  //   var query = connection.query(
-  //     'UPDATE products SET ? WHERE ?',
-  //     [
-  //       {
-  //         stock_quantity: updatedStock,
-  //       },
-  //       {
-  //         ID: chosenProduct
-  //       }
-  //     ], 
-  //     function(err,res) {
-  //       console.log(`Thanks. Your ${orderQty} ${chosenProduct.product_name} have been ordered.`);
-  //     }
-  //   );
-  // };
 
   //sending queries to database & products table
   connection.query('SELECT * from products', function (error, results, fields) {
